228a96c3117b8461d7da3c03cab88b881a9bec7d
SAI SAVERSH R <ssr1032005@gmail.com>
2026-09-02 07:51:25 +0530

    diff model

--- start.bat
+++ start.bat
@@ -3,8 +3,11 @@ echo ==========================================
 echo Starting RepoSense Platform
 echo ==========================================
 
+echo Starting Ollama...
+@REM start "RepoSense Ollama" cmd /c "ollama run llama3.2:3b"
+
 echo Starting Backend Server...
-start "RepoSense Backend" cmd /c "cd backend && pip install -r requirements.txt && py -m src.main"
+start "RepoSense Backend" cmd /c "cd backend && py -m src.main"
 
 echo Starting Frontend Server...
 start "RepoSense Frontend" cmd /c "cd frontend && npm install && npm run dev"
